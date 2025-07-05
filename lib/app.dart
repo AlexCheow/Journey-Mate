adf6e2c7afe22213c75199278c53d3534c8c2ba2
wewmkwm <mwk021101@gmail.com>
2025-07-05 16:17:00 +0800

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,11 @@
+//git checkout WeiMin
 import 'package:flutter/material.dart';
 import 'package:journeymate/screens/navigation.dart';
 import 'screens/login.dart';
 import 'screens/signup.dart';
 import 'screens/verifyemail.dart';
 import 'screens/forgetpassword.dart';
+import 'screens/upcoming_events.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -11,20 +13,21 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'JourneyMate',
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/login',
-      routes: {
-        '/login': (context) => const LoginScreen(),
-        '/home': (context) => const MainNavigation(),
-        '/signup': (context) => const SignUpScreen(),
-        '/verify-email': (context) => const VerifyEmailScreen(),
-        '/forget-password': (context) => const ForgetPasswordScreen(),
-        '/history' : (context) => const MainNavigation(),
-        '/record' : (context) => const MainNavigation(),
-        '/clubs' : (context) => const MainNavigation(),
-        '/profile' : (context) => const MainNavigation(),
-      },
-    );
-  }
+        title: 'JourneyMate',
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/login',
+        routes: {
+    '/login': (context) => const LoginScreen(),
+    '/home': (context) => const MainNavigation(),
+    '/upcoming-events': (context) => const UpcomingEventsPage(),
+    '/signup': (context) => const SignUpScreen(),
+    '/verify-email': (context) => const VerifyEmailScreen(),
+    '/forget-password': (context) => const ForgetPasswordScreen(),
+    '/history' : (context) => const MainNavigation(),
+    '/record' : (context) => const MainNavigation(),
+    '/clubs' : (context) => const MainNavigation(),
+    '/profile' : (context) => const MainNavigation(),
+    },
+        );
+    }
 }
